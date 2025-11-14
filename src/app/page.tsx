3ae877eb0eada18db3222271ecf9a35aec5fcaef
xerdin442 <mudianthonio27@gmail.com>
2025-11-14 20:02:25 +0100

    customized components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,17 @@
+import { Button } from "@/components/ui/button";
+import { Checkbox } from "@/components/ui/checkbox";
+import { Input } from "@/components/ui/input";
+import { Progress } from "@/components/ui/progress";
+import { Textarea } from "@/components/ui/textarea";
+
 export default function Home() {
   return (
-    <div>
-      <h1 className="text-3xl text-rose-600">Hello World</h1>
+    <div className="flex flex-col gap-y-4 p-8 max-w-md">
+      <Button variant="elevated">I am a button</Button>
+      <Input placeholder="I am an input" />
+      <Progress value={50} />
+      <Textarea placeholder="I am a textarea" />
+      <Checkbox />
     </div>
   );
 }
